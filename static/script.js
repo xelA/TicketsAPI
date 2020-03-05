aa6e50d70b11155eb3bfa0562c2f7c4bf7de3186
AlexFlipnote <alexander.samuelsen.manso@gmail.com>
2020-03-05 15:57:40 +0100

    Better support for emojis+links

--- static/script.js
+++ static/script.js
@@ -1,17 +1,22 @@
+function replace_texts(e) {
+  // Find links
+  e.innerHTML = e.innerHTML.replace(
+    /((http|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])?)/ig, (_, url) => `<a class="link" href="${url}" target="_blank">${url}</a>`
+  )
+
+  // Find Discord emojis
+  e.innerHTML = e.innerHTML.replace(
+    /&lt;(a?):([^:]+):(\d+)&gt;/g, (_, a, name, id) => `<img class="emoji" src="https://cdn.discordapp.com/emojis/${id}.${a ? 'gif' : 'png'}" alt="${name}"/>`
+  )
+}
+
 window.onload = function() {
   // Replace all emojis to <img>
   let msg = document.getElementsByClassName("msg")
-  for (var i = 0; i < msg.length; i++) {
-    // Find links
-    msg[i].innerHTML = msg[i].innerHTML.replace(
-      /((http|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])?)/ig, (_, url) => `<a class="link" href="${url}" target="_blank">${url}</a>`
-    )
+  let context = document.getElementById("context")
 
-    // Find Discord emojis
-    msg[i].innerHTML = msg[i].innerHTML.replace(
-      /&lt;(a?):([^:]+):(\d+)&gt;/g, (_, a, name, id) => `<img class="emoji" src="https://cdn.discordapp.com/emojis/${id}.${a ? 'gif' : 'png'}" alt="${name}"/>`
-    )
-  }
+  for (var i = 0; i < msg.length; i++) { replace_texts(msg[i]) }
+  replace_texts(context)
 
   // Enlarge images
   const modal = document.getElementById('modal')
